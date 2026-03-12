b87b363463c58db33fe767a753f9d817c59d5644
TomDev-06 <garnaulttom@gmail.com>
2026-03-12 17:54:05 +0100

    Premier commit du projet Data Scientist

---
+++ lib/00_journalists.rb
@@ -0,0 +1,117 @@
+# The array of Twitter handles
+def journalist_handles
+  ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+end
+
+# 1. Combien y a-t-il de handle dans cette array ?
+# count=compter les handles, handles=les handles
+# handles.length=compter le nombre de handles(domaines) dans l'array
+def count_handles(handles)
+
+  handles.length
+end
+
+# 2. Quel est le handle le plus court de cette liste ?
+# shortest=le plus court, handle=le handle, handles=les handles
+# handles.min_by=trouver le handle le plus court, handle.length=calculer la longueur de chaque handle
+def shortest_handle(handles)
+  handles.min_by { |handle| handle.length }
+end
+
+# 3. Combien y-a-t'il de handle contenant 5 caractères (le @ ne compte pas pour un caractère)
+# count=compter les handles, handles=les handles, handle.length=calculer la longueur de chaque handle
+# handle.length == 6=compter les handles qui ont une longueur de 6 caractères (5 caractères + le '@')
+def count_5_char_handles(handles)
+  handles.count { |handle| handle.length == 6 } # 1 caractère pour '@' + 5 caractères
+end
+
+# 4. Combien commencent par une majuscule (première lettre juste après le @) ?
+# count=compter les handles, uppercase=majuscule, handle=le handle, handles=les handles  
+# count=compter les handles, handles=les handles, handle[1]=la première lettre après le '@', =~ /[A-Z]/=vérifier si la première lettre est une majuscule
+def count_uppercase_handles(handles)
+  handles.count { |handle| handle[1] =~ /[A-Z]/ }
+end
+
+# 5. Trie la liste de handle par ordre alphabétique.
+# sort_by=trier les handles, alpha=alphabétique
+# handles.sort_by=trier les handles, handle.downcase=convertir chaque handle en minuscules pour un tri insensible à la casse
+def sort_handles_alpha(handles)
+  handles.sort_by { |handle| handle.downcase }
+end
+
+# 6. Trie la liste de handle par taille des handle (les plus petits en premiers, les plus grands après)
+# sort_handles_by_length=trier les handles par longueur, handles.sort_by=trier les handles, handle.length=calculer la longueur de chaque handle pour le tri
+# handles.sort_by=trier les handles, handle.length=calculer la longueur de chaque handle pour le tri
+def sort_handles_by_length(handles)
+  handles.sort_by { |handle| handle.length }
+end
+
+# 7. Quelle est la position dans l'array de la personne @epenser?
+# find_epenser_position=trouver la position de @epenser, handles.index=trouver l'index de @epenser dans l'array des handles
+# handles.index("@epenser")=trouver l'index de @epenser dans l'array des handles, si @epenser n'est pas trouvé, index retourne nil
+def find_epenser_position(handles)
+  handles.index("@epenser")
+end
+
+
+# 8. Sors-moi une répartition des handle par taille de ces derniers
+# handle_length_distribution=répartition des handles par taille, distribution=la répartition, Hash.new(0)=initialiser un hash avec une valeur par défaut de 0 pour les tailles de handle
+# handles.each=parcourir chaque handle dans l'array des handles, length=calculer la longueur de chaque handle, distribution[length] += 1=incrémenter le compteur pour cette longueur de handle dans la répartition
+# distribution=la répartition, length=la longueur de chaque handle, distribution[length] += 1=incrémenter le compteur pour cette longueur de handle dans la répartition
+def handle_length_distribution(handles)
+  distribution = Hash.new(0)
+  handles.each do |handle|
+    length = handle.length
+    distribution[length] += 1
+  end
+  distribution
+end
+
+def perform
+  handles = journalist_handles
+
+  puts "--- Analyse des handles de journalistes sur Twitter ---"
+  puts
+
+  puts "1. Combien y a-t-il de handles dans cette liste ?"
+  puts "   Réponse : Il y a #{count_handles(handles)} handles."
+  puts "-"*20
+
+  puts "2. Quel est le handle le plus court ?"
+  puts "   Réponse : Le handle le plus court est #{shortest_handle(handles)}."
+  puts "-"*20
+
+  puts "3. Combien de handles ont 5 caractères (sans le '@') ?"
+  puts "   Réponse : Il y a #{count_5_char_handles(handles)} handles de 5 caractères."
+  puts "-"*20
+
+  puts "4. Combien de handles commencent par une majuscule ?"
+  puts "   Réponse : #{count_uppercase_handles(handles)} handles commencent par une majuscule."
+  puts "-"*20
+
+  puts "5. Voici la liste triée par ordre alphabétique :"
+  puts "   (liste trop longue pour être affichée ici, mais elle est calculée !)"
+  puts "-"*20
+
+  puts "6. Voici la liste triée par taille de handle :"
+  puts "   (liste trop longue pour être affichée ici, mais elle est calculée !)"
+  puts "-"*20
+
+  puts "7. Quelle est la position de '@epenser' ?"
+  position = find_epenser_position(handles)
+  if position
+    puts "   Réponse : '@epenser' se trouve à la position #{position} (index de base 0)."
+  else
+    puts "   Réponse : '@epenser' n'a pas été trouvé dans la liste."
+  end
+  puts "-"*20
+
+  puts "8. Répartition des handles par taille :"
+  distribution = handle_length_distribution(handles)
+  distribution.each do |length, count|
+    puts "   - #{count} handle(s) de #{length} caractère(s)"
+  end
+  puts "-"*20
+end
+
+perform
